b29fe8c822113192a4c5d9c66b49fc9e734c7ba8
Fehmi Yildirim <fy0121@gmail.com>
2026-08-20 12:37:13 +0200

    Implement registration flow

--- src/features/auth/auth.types.ts
+++ src/features/auth/auth.types.ts
@@ -1,7 +1,10 @@
+export type AuthUserStatus = 'PENDING' | 'ACTIVE'
+
 export interface AuthUser {
     id: string
     email: string
     role: string
+    status: string
 }
 
 export interface AuthResponse {
